02dc3185e582ef0ded685e9c23de6c0b22bbdad0
k-wachira <k.wachira@alustudent.com>
2020-05-16 21:41:18 +0200

    improved UI, Added upvote and downvote buttons and their functionality

--- lib/services/display/displayimages.dart
+++ lib/services/display/displayimages.dart
@@ -1,145 +1,172 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
+import 'package:transparent_image/transparent_image.dart';
 import 'package:flutter/material.dart';
 
 class imageloader extends StatelessWidget {
   var dbconn = Firestore.instance;
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return
-      Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
 //          user info
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Row( // comment section
-                  children: <Widget>[
-                    new Container(
-                      height: 40.0,
-                      width: 40.0,
-                      decoration: new BoxDecoration(
-                        shape: BoxShape.circle,
-                        image: new DecorationImage(
-                            fit: BoxFit.fill,
-                            image: new NetworkImage(
-                                document['ImageURL'])),
-                      ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                // comment section
+                children: <Widget>[
+                  new Container(
+                    height: 40.0,
+                    width: 40.0,
+                    decoration: new BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: new DecorationImage(
+                          fit: BoxFit.fill,
+                          image: new NetworkImage(document['ImageURL'])),
                     ),
-                    new SizedBox(
-                      width: 10.0,
-                    ),
-                    new Text(
-                      "imthpk",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    )
-                  ],
-                ),
-                new IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: null,
-                )
-              ],
-            ),
+                  ),
+                  new SizedBox(
+                    width: 10.0,
+                  ),
+                  new Text(
+                    "#memehastag",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  )
+                ],
+              ),
+              new IconButton(
+                icon: Icon(Icons.more_vert),
+                onPressed: null,
+              )
+            ],
           ),
+        ),
 
 //         Main picture
-          Flexible(
+        Flexible(
             fit: FlexFit.loose,
-            child: new Image.network(
-              document['ImageURL'],
+            child: FadeInImage.assetNetwork(
+              placeholder: 'assets/images/loading.gif',
+              image: document['ImageURL'],
               fit: BoxFit.cover,
-            ),
-          ),
+            )),
 
 //          handles like an save comment and share
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                new Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    new Icon(
-                      FontAwesomeIcons.heart,
-                    ),
-                    new SizedBox(
-                      width: 16.0,
-                    ),
-                    new Icon(
-                      FontAwesomeIcons.comment,
-                    ),
-                    new SizedBox(
-                      width: 16.0,
-                    ),
-                    new Icon(FontAwesomeIcons.paperPlane),
-                  ],
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  FlatButton.icon(
+                      icon: Icon(
+                        Icons.arrow_upward,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        document["upvotes"].toString(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        document.reference
+                            .updateData({'upvotes': document['upvotes'] + 1});
+                      }),
+                  FlatButton.icon(
+                      icon: Icon(
+                        Icons.arrow_downward,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        document["downvotes"].toString(),
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        document.reference.updateData(
+                            {'downvotes': document['downvotes'] + 1});
+                      }),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  new SizedBox(
+                    width: 16.0,
+                  ),
+                  new Icon(FontAwesomeIcons.share),
+                ],
+              ),
+              new Icon(FontAwesomeIcons.bookmark)
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            "Tags: #Screenshot #meme #dark humor",
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.blueAccent),
+          ),
+        ),
+
+        //  comment section
+
+        Padding(
+          padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              new Container(
+                height: 40.0,
+                width: 40.0,
+                decoration: new BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: new DecorationImage(
+                      fit: BoxFit.fill,
+                      image: new NetworkImage(document['ImageURL'])),
                 ),
-                new Icon(FontAwesomeIcons.bookmark)
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              "Likes: "+ document["Likes"].toString(),
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ),
-
-
-          //  comment section
-
-          Padding(
-            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                new Container(
-                  height: 40.0,
-                  width: 40.0,
-                  decoration: new BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: new DecorationImage(
-                        fit: BoxFit.fill,
-                        image: new NetworkImage(
-                            document['ImageURL'])),
+              ),
+              new SizedBox(
+                width: 10.0,
+              ),
+              Expanded(
+                child: new TextField(
+                  decoration: new InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Add a comment...",
                   ),
                 ),
-                new SizedBox(
-                  width: 10.0,
-                ),
-                Expanded(
-                  child: new TextField(
-                    decoration: new InputDecoration(
-                      border: InputBorder.none,
-                      hintText: "Add a comment...",
-                    ),
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-
-
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child:
-            Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
-          )
-        ],
-      );
-
-
-
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+        ),
+        SizedBox(
+          height: 5,
+        ),
+        Divider(
+          color: Colors.black12,
+          thickness: 1.2,
+        ),
+        SizedBox(
+          height: 5,
+        ),
+      ],
+    );
   }
 
   @override
