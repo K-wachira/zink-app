4117bdb378086c7ecfcd3e433515676a73184aff
k-wachira <k.wachira@alustudent.com>
2020-05-16 20:00:56 +0200

    added ig like cards to view image _ solved the fetching images realtime

--- lib/services/display/displayimages.dart
+++ lib/services/display/displayimages.dart
@@ -1,18 +1,145 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 import 'package:flutter/material.dart';
 
 class imageloader extends StatelessWidget {
   var dbconn = Firestore.instance;
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
-    return Column(
-      children: <Widget>[
+    return
+      Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
 
-        cd
-      ],
+//          user info
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row( // comment section
+                  children: <Widget>[
+                    new Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: new NetworkImage(
+                                document['ImageURL'])),
+                      ),
+                    ),
+                    new SizedBox(
+                      width: 10.0,
+                    ),
+                    new Text(
+                      "imthpk",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                new IconButton(
+                  icon: Icon(Icons.more_vert),
+                  onPressed: null,
+                )
+              ],
+            ),
+          ),
+
+//         Main picture
+          Flexible(
+            fit: FlexFit.loose,
+            child: new Image.network(
+              document['ImageURL'],
+              fit: BoxFit.cover,
+            ),
+          ),
+
+//          handles like an save comment and share
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Icon(
+                      FontAwesomeIcons.heart,
+                    ),
+                    new SizedBox(
+                      width: 16.0,
+                    ),
+                    new Icon(
+                      FontAwesomeIcons.comment,
+                    ),
+                    new SizedBox(
+                      width: 16.0,
+                    ),
+                    new Icon(FontAwesomeIcons.paperPlane),
+                  ],
+                ),
+                new Icon(FontAwesomeIcons.bookmark)
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Likes: "+ document["Likes"].toString(),
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+          ),
 
 
-    );
+          //  comment section
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0.0, 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                  height: 40.0,
+                  width: 40.0,
+                  decoration: new BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: new DecorationImage(
+                        fit: BoxFit.fill,
+                        image: new NetworkImage(
+                            document['ImageURL'])),
+                  ),
+                ),
+                new SizedBox(
+                  width: 10.0,
+                ),
+                Expanded(
+                  child: new TextField(
+                    decoration: new InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Add a comment...",
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child:
+            Text("1 Day Ago", style: TextStyle(color: Colors.grey)),
+          )
+        ],
+      );
+
+
+
   }
 
   @override
